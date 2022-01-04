7c40767b24d2b53187ea59579e32ebf71165882a
eric <eric.thomas@formation-industries-fc.fr>
2022-01-04 16:07:54 +0100

    constructeur+insererPiece

--- CoffeeMachine.cpp
+++ CoffeeMachine.cpp
@@ -2,10 +2,13 @@
 // Created by eric on 04/01/2022.
 //
 
+#include <iostream>
 #include "CoffeeMachine.h"
 
+using namespace  std;
 
-CoffeeMachine::CoffeeMachine() {
+CoffeeMachine::CoffeeMachine():capacite(3),capacafe(2) {
+
 
 }
 
@@ -14,7 +17,12 @@ void CoffeeMachine::rechargerCafe() {
 }
 
 void CoffeeMachine::insererPiece(float piece) {
-
+if(piece==0.5)
+{
+    servir();
+}
+else
+    throw "Piece invalide";
 }
 
 
--- CoffeeMachine.h
+++ CoffeeMachine.h
@@ -16,7 +16,7 @@ public:
     void insererPiece(float piece); //uniquement piece 0,5
     void rechargerEau(); //recharge la capacité d'eau
     void rechargerCafe(); //recharge la capacité de café
-    void servir(); //sert un café de 0,15cl d'eau et de 10g de café coût du café est 0.5 euros
+    void servir(); //sert un café de 15cl d'eau et de 10g de café coût du café est 0.5 euros
     //si plus de café ou plus d'eau recharge automatique
 
 };
